ff2b16fbf08d5537052e6186278514aa8649eb31
Raúl <rnc0011@alu.ubu.es>
2018-05-27 00:40:33 +0200

    Se ha actualizado el método DefaultExample.
    Issue #9

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -78,10 +78,10 @@ public class Network {
 	public static Network DefaultExample() {
 		Network network = new Network(2);
 
-		Node wsFilip = new Node(Node.WORKSTATION, "Filip");
+		Node wsFilip = new WorkStation("Filip");
 		Node n1 = new Node(Node.NODE, "n1");
-		Node wsHans = new Node(Node.WORKSTATION, "Hans");
-		Node prAndy = new Node(Node.PRINTER, "Andy");
+		Node wsHans = new WorkStation("Hans");
+		Node prAndy = new Printer("Andy");
 
 		wsFilip.nextNode_ = n1;
 		n1.nextNode_ = wsHans;
